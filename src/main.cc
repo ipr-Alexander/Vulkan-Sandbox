9ba0ed4cf9cda9e78b5d4a9fa39fb3979fb9531f
Alexander Marklund <alexander.marklund@impulseradar.com>
2025-09-24 11:31:24 +0200

    Up and running, for now.

--- src/main.cc
+++ src/main.cc
@@ -1,32 +1,81 @@
 #include "vulkan/vulkan.h"
-#include "glm/glm.hpp"
 
+#define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
-    void initVulkan() {
+    void initWindow() {
+        glfwInit();
 
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT,"Vulkan Sandbox", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        createInstance();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.apiVersion = VK_API_VERSION_1_4;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if(result != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create instance!");
+        }
     }
 
     void mainLoop() {
-
+        while(!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+private:
+    GLFWwindow* window;
+    VkInstance instance;
 };
 
 int main() {
@@ -38,6 +87,8 @@ int main() {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
+
+    std::cout << "Hello World" << std::endl;
 
     return EXIT_SUCCESS;
 }
