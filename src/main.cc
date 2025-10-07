8755d69c7d032b0292ac42a00227b4fba6954726
Alexander Marklund <alexander.marklund@impulseradar.com>
2025-10-07 09:00:51 +0200

    implemented Instance

--- src/main.cc
+++ src/main.cc
@@ -6,13 +6,16 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
-class HelloTriangleApplication {
+class HelloTriangleApplication
+{
 public:
-    void run() {
+    void run()
+    {
         initWindow();
         initVulkan();
         mainLoop();
@@ -20,70 +23,115 @@ public:
     }
 
 private:
-    void initWindow() {
+    void initWindow()
+    {
         glfwInit();
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT,"Vulkan Sandbox", nullptr, nullptr);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Sandbox", nullptr, nullptr);
     }
 
-    void initVulkan() {
+    void initVulkan()
+    {
         createInstance();
     }
 
-    void createInstance() {
+    void createInstance()
+    {
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "Hello Triangle";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_4;
 
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
-        
+
+        // --
+        // Optional implementation
+        // --
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available extensions: \n";
+        for (const auto &extension : extensions)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+        //--
+
+        // ----------------------------------------------
+        // -      /////    ///       ////               -
+        // -        /     /   /    /                    -
+        // -        /    /     /    ///                 -
+        // -        /     /   /        //               -
+        // -      /////    ///    ////   implementation -
+        // ----------------------------------------------
+
+        // std::vector<const char *> requiredExtensions;
+
+        // for (uint32_t i = 0; i < glfwExtensionCount; i++)
+        // {
+        //     requiredExtensions.emplace_back(glfwExtensions[i]);
+        // }
+        // requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+        // createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+        // createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+        // createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+        // ----------------------------------------------
+
         uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-        
+        const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
 
-        createInfo.enabledLayerCount = 0;
-
-        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
-        if(result != VK_SUCCESS)
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
         {
             throw std::runtime_error("Failed to create instance!");
         }
     }
 
-    void mainLoop() {
-        while(!glfwWindowShouldClose(window))
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
         {
             glfwPollEvents();
         }
     }
 
-    void cleanup() {
+    void cleanup()
+    {
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
 
 private:
-    GLFWwindow* window;
+    GLFWwindow *window;
     VkInstance instance;
 };
 
-int main() {
+int main()
+{
     HelloTriangleApplication app;
 
-    try {
+    try
+    {
         app.run();
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
